edc33c81ab341bb7ae862568a8838cdfdf5d8f9f
Roberto <info@robertocarraro.com>
2018-07-27 19:52:04 +0200

    Added elapsed time
    Added elapsed time

--- roll8dice.sh
+++ roll8dice.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #
 # Script that rolls n dice and sums their output, m times.
-# Do 10 rolls
 echo "How many rolls?"
 read rolls
 echo "How many dice?"
 read dice
-
+echo "-"
+SECONDS=0
 for (( rolltimes=1; rolltimes<=$rolls; rolltimes++))
-#for rolltimes in {1..1000000}
 do
-   #for i in {1..8}
    for (( dicenumber=1; dicenumber<=$dice; dicenumber++))
       do
          rolled=$(( ( $RANDOM % 6 ) + 1 ))
@@ -23,11 +21,11 @@ do
    # Clear $sum
    unset sum
 done
-
+ 
 # Output the total of sums
 for (( result=$dice; result<=$dice*6; result++ ))
-#for result in {8..48}
 do
    echo "Result "$result": "$((total$result))
 done
-
+echo "-"
+echo "Elapsed time: "$SECONDS" seconds."

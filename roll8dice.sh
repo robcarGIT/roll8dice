7cdf0e9461f153d6da0a875be9937d75b51b0cfc
Roberto <info@robertocarraro.com>
2018-08-02 21:31:09 +0200

    Added integer-only input check
    Added integer-only input check.

--- roll8dice.sh
+++ roll8dice.sh
@@ -3,8 +3,20 @@
 # Script that rolls n dice and sums their output, m times.
 echo "How many rolls?"
 read rolls
+# Check if input is integer only
+if ! [[ "$rolls" =~ ^[0-9]+$ ]]
+    then
+        echo "Sorry, integers only"
+        exit 1
+fi
 echo "How many dice?"
 read dice
+# Check if input is integer only
+if ! [[ "$dice" =~ ^[0-9]+$ ]]
+    then
+        echo "Sorry, integers only"
+        exit 1
+fi
 echo "-"
 SECONDS=0
 for (( rolltimes=1; rolltimes<=$rolls; rolltimes++))
